1ba9b9d8bf9f47d4ea55b9651e7eba4286d5b98a
Gaurav Malik <39573289+gauravmalik1388@users.noreply.github.com>
2020-04-11 14:31:13 -0400

    Updates

---
+++ precache-manifest.f1bd227cb543dbd17e545d3d096373c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc9091c24534b56cb6802d95d322a88",
+    "url": "/Robo-Friends/index.html"
+  },
+  {
+    "revision": "dd19ad8a2babc5737b09",
+    "url": "/Robo-Friends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "201fe797963659faca61",
+    "url": "/Robo-Friends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dd19ad8a2babc5737b09",
+    "url": "/Robo-Friends/static/js/2.843b3040.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robo-Friends/static/js/2.843b3040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "201fe797963659faca61",
+    "url": "/Robo-Friends/static/js/main.61b669b5.chunk.js"
+  },
+  {
+    "revision": "7d544884f4b13b4c82be",
+    "url": "/Robo-Friends/static/js/runtime-main.1b0f526c.js"
+  }
+]);
